ac64942aef9a631397bb60f4a3e69ce77dbce23c
ferlobita <lobainamarifer@gmail.com>
2022-11-08 18:26:41 -0600

    subir 2

---
+++ tarea2/Operators.java
@@ -0,0 +1,22 @@
+public class Operators {
+  public static void main(String[] args) {
+    // declare variables
+    double a = 20.05, b = 5.32;
+    double c=0
+    // addition operator
+    System.out.println(x= "a + b = ");
+    c = (a + b);
+    
+    // subtraction operator
+    System.out.println("a - b = " + (a - b));
+
+    // multiplication operator
+    System.out.println("a * b = " + (a * b));
+
+    // division operator
+    System.out.println("a / b = " + (a / b));
+
+    // modulo operator
+    System.out.println("a % b = " + (a % b));
+  }
+}
